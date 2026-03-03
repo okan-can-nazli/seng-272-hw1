3ec40c96bd1a7890787ea3ea82df6e23b3313796
okan-can-nazli <okancanazli@gmail.com>
2026-03-03 12:38:46 +0300

    Add files via upload

---
+++ LAB1-SWING-FILEIO/src/ProjectFormApp.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class ProjectFormApp {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Software Project Registration Page");
+        frame.setSize(500,450);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new ProjectFormPanel());
+        frame.setVisible(true);
+    }
+    
+}
